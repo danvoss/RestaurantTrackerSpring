3e705f7059577976c34962e1348554e27e1cac91
Dan <danielandrewvoss@gmail.com>
2016-06-21 10:47:48 -0400

    Connect to db and create tables

---
+++ src/main/java/com/dvoss/User.java
@@ -0,0 +1,26 @@
+package com.dvoss;
+
+import javax.persistence.*;
+
+/**
+ * Created by Dan on 6/21/16.
+ */
+@Entity // <-- always use in order to have db
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue
+    int id;
+
+    @Column(nullable = false) // tell db these fields can't be null to prevent bad data down the line
+    String name;
+
+    @Column(nullable = false)
+    String password;
+
+    public User(String name, String password) {
+        // remember, the db generates id, so absent from constructor
+        this.name = name;
+        this.password = password;
+    }
+}
