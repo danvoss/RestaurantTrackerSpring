ebcb3d1aca555dc2029bb7e238b3390d851b633a
Dan <danielandrewvoss@gmail.com>
2016-06-22 12:10:00 -0400

    Add query methods and join

--- src/main/java/com/dvoss/Restaurant.java
+++ src/main/java/com/dvoss/Restaurant.java
@@ -24,13 +24,18 @@ public class Restaurant {
     @Column(nullable = false)
     String comment;
 
+    // join to user:
+    @ManyToOne
+    User user;
+
     public Restaurant() {
     }
 
-    public Restaurant(String name, String location, int rating, String comment) {
+    public Restaurant(String name, String location, int rating, String comment, User user) {
         this.name = name;
         this.location = location;
         this.rating = rating;
         this.comment = comment;
+        this.user = user;
     }
 }
--- src/main/java/com/dvoss/RestaurantRepository.java
+++ src/main/java/com/dvoss/RestaurantRepository.java
@@ -1,9 +1,23 @@
 package com.dvoss;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 /**
  * Created by Dan on 6/21/16.
  */
 public interface RestaurantRepository extends CrudRepository<Restaurant, Integer> {
+    public Iterable<Restaurant> findByRating(int rating);
+    public Iterable<Restaurant> findByLocation(String location);
+    public Iterable<Restaurant> findByRatingAndLocation(int rating, String location);
+    public Iterable<Restaurant> findByRatingGreaterThanEqual(int rating);
+
+    public int countByLocation(String location);
+
+    public Iterable<Restaurant> findByOrderByRatingDesc(); // no args bc ordering, not filtering
+
+    public Iterable<Restaurant> findByUser(User user);
+
+    @Query("SELECT r FROM Restaurant r WHERE r.location LIKE ?1%") // must use alias ("r") & table name must be class name
+    public Iterable<Restaurant> searchLocation(String location);
 }
